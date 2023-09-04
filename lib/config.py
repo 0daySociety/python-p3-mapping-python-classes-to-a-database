6c55b0a5c2b4ae81f5ed51df2e6379841c6b7282
kimemia <david.thuku@student.moringaschool.com>
2023-09-04 01:48:29 -0700

    done

--- lib/config.py
+++ lib/config.py
@@ -1,4 +1,3 @@
 import sqlite3
-
-CONN = sqlite3.connect('music.db')
-CURSOR = CONN.cursor()
+CONN =sqlite3.connect('db/music.db')
+CURSOR=CONN.cursor()
---
+++ lib/kimemia.py
@@ -0,0 +1,22 @@
+from config import CONN, CURSOR
+class Song:
+    def __init__(self,name,album):
+        self.id=None
+        self.name=name
+        self.album=album
+    @classmethod
+    def create_table(self):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            ) 
+        """
+        CURSOR.execute(sql)
+      
+        
+
+
+manny=Song.create_table()
+print(manny)
